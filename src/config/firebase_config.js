74fd6286e0268b7965d3c1b7204910f84895d604
Testerrocks4634 <88300669+Testerrocks4634@users.noreply.github.com>
2021-08-02 09:38:39 +0530

    Add files via upload

---
+++ src/config/firebase_config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD2e8j_p8NH_YNhvtpk3sy8sd5KUS1uFgU",
+    authDomain: "apps-691e2.firebaseapp.com",
+    projectId: "apps-691e2",
+    storageBucket: "apps-691e2.appspot.com",
+    messagingSenderId: "639791732542",
+    appId: "1:639791732542:web:1e1b8425756a813dac3e6f",
+    measurementId: "G-QN6CPTQ4NB"
+  };
+exports.config = firebaseConfig;
